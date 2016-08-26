1ecfe5c23e373ec57723cdbf10e5d1d4bb652c3c
Mike Bauders <mike.bauders@gmail.com>
2016-08-26 11:07:50 -0400

    v2
    Updated working code with full comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function creates a cached inverse matrix to potentially speed up
+## processing time when calculating matrix inverses
+## Function is based on the sample code provided in assignment instructions
 
 makeCacheMatrix <- function(x = matrix()) {
-     invMat <- NULL
-     set <- function(y) {
+     invMat <- NULL             ##declares empty inverse matrix variable
+     set <- function(y) {         ##sets matrix
           x <<- y
           invMat <<- NULL
      }
-     get <- function() x
-     setinv <- function(inverse) invMat <<- inverse
-     getinv <- function() invMat
+     get <- function() x          ##acquires matrix
+     setinv <- function(inverse) invMat <<- inverse   ##sets inverse matrix
+     getinv <- function() invMat     ##gets inverse matrix
      list(set = set, get = get, setinv = setinv, getinv = getinv)
+     ##creates list to be accessed by the cacheSolve function
     
 }
 
 
-## Write a short comment describing this function
-new
+##this function retrieves the data from the above function and returns the
+##inverse matrix or calculates the inverse matrix if it has yet to be created
+
 cacheSolve <- function(x, ...) {
-        invMat <- x$getinv()
+        invMat <- x$getinv()   ##retrieves inverse from list in above function 
         
-        if(!is.null(invMat)){
-            print("getting cached data")
-            return(invMat)
+        if(!is.null(invMat)){        ##if inverse is already calcuclated it is
+            print("getting cached data")   ##retrieved by this loop and returned
+            return(invMat)                
         }
-        matrix <- x$get()
-        invMat <- solve(matrix, ...)
-    x$setinv(invMat)
-return(invMat)
+        matrix <- x$get()       ##if inverse is not yet calculated, the matrix
+        invMat <- solve(matrix, ...)   ##is called from the list created in
+    x$setinv(invMat)                   ##earlier function and solved.
+return(invMat)     ##inverse matrix is returned
 }
